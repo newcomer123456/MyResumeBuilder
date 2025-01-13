9ada035b8ecc5aabbff94c9061075b985990846a
newcomer123456 <157855469+newcomer123456@users.noreply.github.com>
2025-01-13 19:45:33 +0200

    commit

--- resume_builder/settings.py
+++ resume_builder/settings.py
@@ -134,3 +134,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'auto.CustomUser'
+
+LOGIN_URL = '/login/'  # Змінити на ваш маршрут для входу
+LOGIN_REDIRECT_URL = '/'
